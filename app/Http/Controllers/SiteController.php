04d4ad8e7d41f2c3b2e2a812e638d94b6281adc7
Romina Suarez <rowasc@gmail.com>
2020-03-26 21:23:46 -0300

    Fix neighbors vs public visibility issue

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Block;
+
 use App\BlockTypeFields;
 use App\Minisite;
 use Illuminate\Http\Request;
@@ -17,25 +19,32 @@ class SiteController extends Controller
         if (!$this->canSeeMinisite($minisite)) {
             abort(403, 'The site is not available.');
         }
-        foreach($minisite->blocks as &$block) {
+        $returnBlocks = [];
+        foreach($minisite->blocks as $block) {
             $content = json_decode($block->content);
             $mapped = [];
-            if ($block->type === 'Featured Youtube Video') {
-                
-            }
             foreach ($content as $field_key => $field_value) {
                 $fieldDefinition = BlockTypeFields::where(['id' => (int) $field_key])->first();
                 //@todo when less asleep: make into a transformer setup of sorts
-                if ($block->type === 'Featured Youtube Video' && $fieldDefinition->name === 'Url') {
-                    preg_match('/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/', $field_value, $youtubeId);
-                    // @todo: do this in the youtube video creation to validate that it's in fact a youtube url? 
-                    $field_value = isset($youtubeId[1]) ? $youtubeId[1] : $field_value;
-                }
+                $field_value = $this->transform($block, $fieldDefinition, $field_value);
                 $mapped[$fieldDefinition->name] = $field_value;
             }
             $block->content = $mapped;
+            if ($this->canSeeBlock($block)) {
+                $returnBlocks[] = $block;
+            }
         }
+        $minisite->blocks = $returnBlocks;
         return view('site.public', ['minisite' => $minisite]);
+    }
+    
+    private function transform(Block $block, BlockTypeFields $fieldDefinition, $field_value) {
+        if ($block->type === 'Featured Youtube Video' && $fieldDefinition->name === 'Url') {
+            preg_match('/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/', $field_value, $youtubeId);
+            // @todo: do this in the youtube video creation to validate that it's in fact a youtube url? 
+            $field_value = isset($youtubeId[1]) ? $youtubeId[1] : $field_value;
+        }
+        return $field_value;
     }
 
     private function canSeeMinisite(Minisite $minisite) {
@@ -43,8 +52,20 @@ class SiteController extends Controller
             return true;
         }
         $user = Auth::user();
-        $isNeighbor = $user && $user->neighborhood_id === $minisite->neighborhood_id;
+        $isNeighbor = $user && $user->neighborhood_id && $user->neighborhood_id === $minisite->neighborhood_id;
         if ($minisite->visibility === 'neighbor' && $isNeighbor === true) {
+            return true;
+        }
+        return false;
+    }
+    
+    private function canSeeBlock(Block $block) {
+        if ($block->visibility === 'public') {
+            return true;
+        }
+        $user = Auth::user();
+        $isNeighbor = $user && $user->neighborhood_id && $user->neighborhood_id === $block->neighborhood_id;
+        if ($block->visibility === 'neighbor' && $isNeighbor === true) {
             return true;
         }
         return false;
