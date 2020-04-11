ecb744c2f48519caa2a8c2e50506ca7b9e8057f0
rowasc <rowasc@gmail.com>
2020-04-11 03:04:37 +0000

    Reorder blocks functionality - fix

--- Modules/Minisite/Resources/views/components/page-header.blade.php
+++ Modules/Minisite/Resources/views/components/page-header.blade.php
@@ -1,6 +1,6 @@
 <div class="mdc-layout-grid__cell--span-12">
     <div class="mdc-card">
-        {{$block->content->Title}}
-        {{$block->content->Description}}
+        <h1>{{$block->content->Title}}</h1>
+        <p>{{$block->content->Description}}</p>
     </div>
 </div>
