548d8f450fd9de64a9f9830ea2adb2b7c5e5a9d4
rowasc <rowasc@gmail.com>
2020-04-12 18:55:55 +0000

    Add community context menu items - site settings + bugfix for login button

--- app/Http/Middleware/SetCurrentCommunity.php
+++ app/Http/Middleware/SetCurrentCommunity.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class SetCurrentCommunity
 {
@@ -15,9 +17,13 @@ class SetCurrentCommunity
      */
     public function handle($request, Closure $next)
     {
-        if ($request->route()->community) {
+        if ($request->route()->community && Auth::user()) {
             session(['selectedCommunityName' => $request->route()->community->name]);
             session(['selectedCommunitySlug' => $request->route()->community->slug]);
+        }
+        if (session('selectedCommunityName') && !Auth::user()) {
+            session(['selectedCommunityName' => null ]);
+            session(['selectedCommunitySlug' => null ]);
         }
         return $next($request);
     }
--- resources/views/includes/menu-context.blade.php
+++ resources/views/includes/menu-context.blade.php
@@ -5,102 +5,106 @@
             <div class="mdc-layout-grid__cell--span-12">
                 <h3 class="mdc-drawer__title">Mahallah.org</h3>
             </div>
+            @endif
             @if (!Auth::user())
-            <div class="mdc-layout-grid__cell--span-12">
-                <div class="button-group">
-                    <div class="mdc-layout-grid__inner">
-                        <div class="grid-cell">
-                            <a href="{{ route('login') }}" class="mdc-button mdc-button--raised">
-                                <div class="mdc-button__ripple"></div>
-                                <span class="mdc-button__label">@lang('auth.login')</span>
-                            </a>
-                        </div>
-                        <div class="grid-cell">
-                            <a href="{{ route('register') }}" class="mdc-button mdc-button--raised">
-                                <div class="mdc-button__ripple"></div>
-                                <span class="mdc-button__label">@lang('auth.register')</span>
-                            </a>
+                <div class="mdc-layout-grid__cell--span-12">
+                    <div class="button-group">
+                        <div class="mdc-layout-grid__inner">
+                            <div class="grid-cell">
+                                <a href="{{ route('login') }}" class="mdc-button mdc-button--raised">
+                                    <div class="mdc-button__ripple"></div>
+                                    <span class="mdc-button__label">@lang('auth.login')</span>
+                                </a>
+                            </div>
+                            <div class="grid-cell">
+                                <a href="{{ route('register') }}" class="mdc-button mdc-button--raised">
+                                    <div class="mdc-button__ripple"></div>
+                                    <span class="mdc-button__label">@lang('auth.register')</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            @endif
             @else
-            <div class="mdc-layout-grid__cell--span-12">
-                <div class="avatar">
-                    <i class="fas fa-user-circle icon"></i>
+                <div class="mdc-layout-grid__cell--span-12">
+                    <div class="avatar">
+                        <i class="fas fa-user-circle icon"></i>
+                    </div>
                 </div>
-            </div>
-            <div class="mdc-layout-grid__cell--span-12">
-                <div class="content">
-                    <h2 class="mdc-card__title mdc-typography mdc-typography--headline6 p-0">
-                        {{ Auth::user() ? Auth::user()->name : '' }}
-                    </h2>
-                    
-                    <span class="mdc-card__subtitle mdc-typography mdc-typography--subtitle2">
-                        {{ Auth::user() ? Auth::user()->email : '' }}
-                        <i class="fas fa-caret-down icon"></i>
+                <div class="mdc-layout-grid__cell--span-12">
+                    <div class="content">
+                        <h2 class="mdc-card__title mdc-typography mdc-typography--headline6 p-0">
+                            {{ Auth::user() ? Auth::user()->name : '' }}
+                        </h2>
+                        
+                        <span class="mdc-card__subtitle mdc-typography mdc-typography--subtitle2">
+                            {{ Auth::user() ? Auth::user()->email : '' }}
+                            <i class="fas fa-caret-down icon"></i>
 
-                        <!-- @seth make this link pink??? -->
-                        {{-- <a href="{{ route('login') }}" class="">
-                            <div class="mdc-button__ripple"></div>
-                            <span class="mdc-button__label">@lang('auth.login')</span>
-                        </a> --}}
-                    </span>
+                            <!-- @seth make this link pink??? -->
+                            {{-- <a href="{{ route('login') }}" class="">
+                                <div class="mdc-button__ripple"></div>
+                                <span class="mdc-button__label">@lang('auth.login')</span>
+                            </a> --}}
+                        </span>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
-        @endif
     </div>
 </div>
 </div>
 <div class="mdc-drawer__content">
     <nav class="mdc-list">
         @if (Auth::user() && session('selectedCommunitySlug'))
-        <a class="mdc-list-item" href="{{route('minisite.admin' , ['community' => session('selectedCommunitySlug') ])}}" aria-current="page">
-            <span class="mdc-list-item__text"> < {{session('selectedCommunityName')}} > </span>
-        </a>
-        <a class="mdc-list-item" href="{{ route('blockTypes', ['community' => session('selectedCommunitySlug')]) }}" aria-current="page">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">Add Blocks</span>
-        </a>
-        <a class="mdc-list-item" href="{{ route('minisite.admin.reorder', ['community' => session('selectedCommunitySlug')]) }}">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">Reorder Blocks</span>
-        </a>
-        {{-- <a class="mdc-list-item" href="/manage-members">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">Manage Members</span>
-        </a> --}}
+            <a class="mdc-list-item" href="{{route('minisite.admin' , ['community' => session('selectedCommunitySlug') ])}}" aria-current="page">
+                <span class="mdc-list-item__text"> < {{session('selectedCommunityName')}} > </span>
+            </a>
+            <a class="mdc-list-item" href="{{ route('blockTypes', ['community' => session('selectedCommunitySlug')]) }}" aria-current="page">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Add Blocks</span>
+            </a>
+            <a class="mdc-list-item" href="{{ route('minisite.admin.reorder', ['community' => session('selectedCommunitySlug')]) }}">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Reorder Blocks</span>
+            </a>
+            {{-- <a class="mdc-list-item" href="/manage-members">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Manage Members</span>
+            </a> --}}
+            <a class="mdc-list-item" href="{{ route('communityEdit', ['community' => session('selectedCommunitySlug')]) }}">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Site Settings</span>
+            </a>
         @endif
         @if(Auth::user())
-        <hr class="mdc-list-divider">
-        <a tabindex="0" class="mdc-list-item" href="{{route('home')}}" aria-current="page">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">My communities</span>
-        </a>
-        @endif
-        <a class="mdc-list-item" href="{{route('communityCreate')}}" aria-current="page">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">Create a community</span>
-        </a>
-        <a class="mdc-list-item" href="{{route('landing.search')}}">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">Search</span>
-        </a>
-        <a class="mdc-list-item" href="/">
-            <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">About Mahalla</span>
-        </a>
-        @if (Auth::user())
-        <a class="mdc-list-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            <i class="fas fa-sign-out-alt mdc-list-item__graphic" aria-hidden="true"></i>
-            <span class="mdc-list-item__text">{{ __('Logout') }}</span>
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
+            <hr class="mdc-list-divider">
+            <a tabindex="0" class="mdc-list-item" href="{{route('home')}}" aria-current="page">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">My communities</span>
+            </a>
+            @endif
+            <a class="mdc-list-item" href="{{route('communityCreate')}}" aria-current="page">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Create a community</span>
+            </a>
+            <a class="mdc-list-item" href="{{route('landing.search')}}">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">Search</span>
+            </a>
+            <a class="mdc-list-item" href="/">
+                <i class="fas fa-heart mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">About Mahalla</span>
+            </a>
+            @if (Auth::user())
+            <a class="mdc-list-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt mdc-list-item__graphic" aria-hidden="true"></i>
+                <span class="mdc-list-item__text">{{ __('Logout') }}</span>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
         @endif
     </nav>
 </div>
