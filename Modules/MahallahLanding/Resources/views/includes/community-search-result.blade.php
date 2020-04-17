022a8acfda5f253c114e26d54c6a099720af1caa
Seth Hall <sethburtonhall@gmail.com>
2020-04-17 12:23:27 -0400

    fixes styling issue with a class change
    `theme--primary-bg` to `theme-primary-bg`

--- Modules/MahallahLanding/Resources/views/includes/community-search-result.blade.php
+++ Modules/MahallahLanding/Resources/views/includes/community-search-result.blade.php
@@ -5,7 +5,7 @@
             <div class="mdc-card__media mdc-card__media--16-9 demo-card__media"
                 style="background-image: url('https://source.unsplash.com/WLUHO9A_xik/1600x900');">
             </div>
-            <div class="card__primary theme--primary-bg community-card-location">
+            <div class="card__primary theme-primary-bg community-card-location">
                 <p class="card__subtitle mdc-typography mdc-typography--subtitle2">{{ $community->country }}</p>
                 <p class="card__subtitle mdc-typography mdc-typography--subtitle2">{{ $community->name }}</p>
             </div>
