e8bc319dcf34471429b96f84c014216649749367
rowasc <rowasc@gmail.com>
2020-04-19 05:38:07 +0000

    Fix routes

--- Modules/Minisite/Routes/web.php
+++ Modules/Minisite/Routes/web.php
@@ -10,47 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/site/{community}', 'MinisiteController@index')->name('minisite.admin');
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/{community}/reorder', 'MinisiteController@reorder')->name('minisite.admin.reorder');
-    Route::post('/{community}/reorder', 'MinisiteController@saveOrder')->name('minisite.admin.reorder.submit');
-
-    Route::get('/{community}/block/types', 'BlockController@getTypes')->name('blockTypes');
-
-    Route::get('/{community}/block/create', 'BlockController@create')->name('createByType');
-
-    Route::put('/{community}', 'MinisiteController@update')->name('communityUpdate');
-
-    // blocks
-    // Route::get('/{community}/block/create', 'BlockController@create')->name('blockCreate');
-    Route::post('/{community}/block', 'BlockController@store')->name('blockStore');
-    Route::get('/{community}/block/{block}/edit', 'BlockController@edit')->name('blockEdit');
-    Route::put('/{community}/block/{block}', 'BlockController@update')->name('blockUpdate');
-    Route::get('/{community}/block/{block}/destroy', 'BlockController@destroy')->name('blockDestroy');
-});
-
-Route::prefix('communitymanager')->group(function() {
-    
-    Route::get('/user/invited/{token}', 'CommunityManagerController@joinFromInvite')->name('joinFromInvite');
-
-});
-Route::post('/{community}/{block}/email', 'CommunityManagerController@email')->name('sendSiteEmail');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', 'CommunityManagerController@all')->name('home');
     Route::prefix('community')->group(function () {
-        Route::get('/create', 'CommunityManagerController@create')->name('communityCreate');
-        Route::get('/{community}/edit', 'CommunityManagerController@edit')->name('communityEdit');
-
-        Route::post('/', 'CommunityManagerController@store')->name('communityStore');
-        Route::put('/{community}', 'CommunityManagerController@update')->name('communityUpdate');
-
-        Route::get('/{community}/location', 'CommunityManagerController@getLocationOptions')->name('getLocationOptions');
-        Route::post('/{community}/location', 'CommunityManagerController@storeLocation')->name('communitySetLocation');
-        Route::get('/', 'CommunityManagerController@show')->name('communityShow');    
-        Route::get('/{id}', 'CommunityManagerController@show')->name('communityShow');
-
+        // order blocks
+        Route::get('/{community}/reorder', 'MinisiteController@reorder')->name('minisite.admin.reorder');
+        Route::post('/{community}/reorder', 'MinisiteController@saveOrder')->name('minisite.admin.reorder.submit');
 
         //invite mgmt
         Route::get('/{community}/members/invite', 'InviteController@inviteMembers')->name('community.members.invite');
@@ -60,6 +27,28 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/{community}/members', 'MembershipManagerController@members')->name('community.members');
         Route::get('/{community}/members/{user}', 'MembershipManagerController@member')->name('community.member');
         Route::put('/{community}/members/{user}', 'MembershipManagerController@updateMember')->name('community.member.update');
+        
+        // manage blocks
+
+        Route::get('/{community}/block/{block}/edit', 'BlockController@edit')->name('blockEdit');
+        Route::get('/{community}/block/{block}/destroy', 'BlockController@destroy')->name('blockDestroy');
+        Route::get('/{community}/block/types', 'BlockController@getTypes')->name('blockTypes');
+        Route::get('/{community}/block/create', 'BlockController@create')->name('createByType');
+        Route::put('/{community}/block/{block}', 'BlockController@update')->name('blockUpdate');
+        Route::post('/{community}/block', 'BlockController@store')->name('blockStore');
+        Route::get('/{community}/edit', 'CommunityManagerController@edit')->name('communityEdit');
+        Route::get('/{community}/location', 'CommunityManagerController@getLocationOptions')->name('getLocationOptions');
+        Route::post('/{community}/location', 'CommunityManagerController@storeLocation')->name('communitySetLocation');
+        Route::get('/create', 'CommunityManagerController@create')->name('communityCreate');
+        Route::put('/{community}', 'MinisiteController@update')->name('communityUpdate');
+        Route::post('/', 'CommunityManagerController@store')->name('communityStore');
+
 
     });
+});
+
+Route::prefix('community')->group(function() {
+    Route::get('/{community}', 'MinisiteController@index')->name('minisite.admin');
+    Route::get('/user/invited/{token}', 'CommunityManagerController@joinFromInvite')->name('joinFromInvite');
+    Route::post('/{community}/{block}/email', 'CommunityManagerController@email')->name('sendSiteEmail');
 });
