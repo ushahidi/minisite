c111236f9d6bb285b0cb2be4b36266b690cd70b3
Romina <rowasc@gmail.com>
2020-04-18 21:22:05 -0300

    Community moved to Minisite module along with blocks

--- Modules/Minisite/Routes/web.php
+++ Modules/Minisite/Routes/web.php
@@ -29,3 +29,33 @@ Route::group(['middleware' => 'auth'], function () {
     Route::put('/{community}/block/{block}', 'BlockController@update')->name('blockUpdate');
     Route::get('/{community}/block/{block}/destroy', 'BlockController@destroy')->name('blockDestroy');
 });
+
+Route::prefix('communitymanager')->group(function() {
+    
+    Route::get('/user/invited/{token}', 'CommunityManagerController@joinFromInvite')->name('joinFromInvite');
+
+});
+Route::post('/{community}/{block}/email', 'CommunityManagerController@email')->name('sendSiteEmail');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'CommunityManagerController@all')->name('home');
+    Route::prefix('community')->group(function () {
+        Route::get('/create', 'CommunityManagerController@create')->name('communityCreate');
+        Route::get('/{community}/edit', 'CommunityManagerController@edit')->name('communityEdit');
+        
+        Route::post('/{communityId}/invite', 'InviteController@generate')->name('inviteMember');
+        Route::post('/', 'CommunityManagerController@store')->name('communityStore');
+        Route::put('/{community}', 'CommunityManagerController@update')->name('communityUpdate');
+
+        Route::get('/{community}/location', 'CommunityManagerController@getLocationOptions')->name('getLocationOptions');
+        Route::post('/{community}/location', 'CommunityManagerController@storeLocation')->name('communitySetLocation');
+        Route::get('/', 'CommunityManagerController@show')->name('communityShow');    
+        Route::get('/{id}', 'CommunityManagerController@show')->name('communityShow');    
+        Route::get('/{community}/members', 'CommunityManagerController@members')->name('community.members');
+        Route::get('/{community}/members/invite', 'CommunityManagerController@inviteMembers')->name('community.members.invite');
+        Route::post('/{community}/members/invite', 'CommunityManagerController@sendInvites')->name('community.members.invite.send');
+        Route::get('/{community}/members/{user}', 'CommunityManagerController@member')->name('community.member');
+        Route::put('/{community}/members/{user}', 'CommunityManagerController@updateMember')->name('community.member.update');
+
+    });
+});
